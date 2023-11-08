9b9e698e6cd15be568865728613ee4d36eebe630
Priya <Ps975076@gmail.com>
2023-11-08 11:44:02 +0000

    got the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import './App.css';
+import "./App.css";
+
+// 1. Import useState, useEffect
+import { useState, useEffect } from "react";
 
 function App() {
-    return (
-        <p>Hello, world!</p>
-    );
+  // 2. Create States
+  const [contactList, setContactList] = useState([]);
+  const [addANewContact, setAddANewContact] = useState([]);
+
+  // 4. useEffect
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // 3. Fetch the data
+  const fetchData = () => {
+    fetch("https://boolean-api-server.fly.dev/ps975076/contact")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, "data");
+        setContactList(data);
+      });
+  };
+
+  return <h2>Menu</h2>;
 }
 
 export default App;
